a7f826dabcabe98cf80224a65b6f32a798ed4440
pross1312 <dangvinhtuong12@gmail.com>
2024-12-02 22:15:15 +0700

    fix google integration, read token from urlparams

--- src/component/GoogleCallback.tsx
+++ src/component/GoogleCallback.tsx
@@ -1,31 +1,22 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
-function deleteCookie(name: string): void {
-  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
-}
-
-function getValueFromCookie(name: string): string | null {
-  const pairs = document.cookie.split(";");
-  for (let i=0; i < pairs.length; i++) {
-    const pair = pairs[i].split("=", 2);
-    if (pair[0].trim() === name) {
-      return pair[1];
-    }
-  }
-  return null;
-}
-
 export default function() {
+
   const authContext = useAuth();
   if (authContext.isAuthenticated()) {
     return (
       <Navigate to="/home" replace />
     );
   } else {
-    const token = getValueFromCookie('google-token');
-    authContext.setAccessTokenWithoutRerender(token);
-    deleteCookie('google-token');
-    return <Navigate to="/home" replace />;
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+    if (!token) {
+      console.log("Server didn't send any token");
+      return <Navigate to="/login" replace />;
+    } else {
+      authContext.setAccessTokenWithoutRerender(token);
+      return <Navigate to="/home" replace />;
+    }
   }
 }
